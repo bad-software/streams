7ad0156f4a8164e0dc28a967b23c81a8a3954b13
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2023-10-16 20:03:57 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+export { compressBlob } from './compressBlob.js'
+export { compressStream } from './compressStream.js'
+export { decompressBlob } from './decompressBlob.js'
+export { decompressStream } from './decompressStream.js'
+export { pipeStreamToFile } from './pipeStreamToFile.js'
+export { saveStream } from './saveStream.js'
+export { saveCompressedBlob } from './saveCompressedBlob.js'
---
+++ saveStream.js
@@ -0,0 +1,34 @@
+import { pipeStreamToFile } from './pipeStreamToFile.js'
+
+
+const types = [
+  {
+    description: 'Zine',
+    accept: {
+      'application/octet-stream': ['.zine'],
+    }
+  }
+]
+
+
+/**
+ * Save a stream by piping it to a file.
+ * @param {ReadableStream} stream The stream to save.
+ * @param {string} filename The suggested filename.
+ * @param {string} id The file picker ID.
+ * @returns {Promise<FileSystemFileHandle>} The file handle.
+ */
+export async function saveStream(
+  stream = new ReadableStream(),
+  filename = 'file.bin',
+  id = 'download',
+) {
+  const handle = await window.showSaveFilePicker({
+    types,
+    suggestedName: filename,
+    id: 'zine',
+  })
+
+  await pipeStreamToFile( stream, handle )
+  return handle
+}
